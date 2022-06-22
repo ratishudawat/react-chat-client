d47354c49a70498fe879c001c376d8469e8bc7db
Ratish Udawat <ratishudawat@gmail.com>
2022-06-22 07:53:23 +0530

    TeamChannelList component added

--- src/components/ChannelListContainer.js
+++ src/components/ChannelListContainer.js
@@ -33,6 +33,11 @@ function ChannelListContainer() {
       <div className='channel-list__list__wrapper'>
         <CompanyHeader />
         <ChannelSearch />
+        <ChannelList
+          filters={{}}
+          channelRenderFilterFn={() => {}}
+          List={(listProps) => <TeamChannelList {...listProps} type='team' />}
+        />
       </div>
     </>
   );
